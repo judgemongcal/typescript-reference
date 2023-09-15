c386371058111fdfebba5c424411bb4cb0c874d1
Judge Mongcal <134949328+judgemongcal@users.noreply.github.com>
2023-09-15 20:35:37 +0800

    Type Alias

--- 5-object-types/objects.ts
+++ 5-object-types/objects.ts
@@ -14,9 +14,29 @@ function randomCoords () : {x:number, y:number} { // Valid; for multiple return 
 
 // Order of arguments passed can be not in the same order as defined in the function params
 
+
+
 // Excess Properties
 printName({first: 'John', last: 'Doe', age: 41}); // Inline: Will have error since age is not defined in the function params
 
 const singer = {first: 'John', last: 'Doe', age: 41};
 
 printName(singer); // Will work but TS will just get the needed arguments for the function's params and just neglect the other args.
+
+
+
+
+
+// Type Alias - useful for objects
+
+type Course = {
+    name: string,
+    instructor: string,
+    duration: number
+};
+
+const checkCourse = (entry : Course) => { //Used reference type named Course
+    return `The course is called ${entry.name}, and it is taught by ${entry.instructor}. It is ${entry.duration} hours long`;
+}
+
+checkCourse({name: 'TypeScript 2023', duration: 10, instructor: 'Colt Steele'});
