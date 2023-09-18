50942ea91bef501e9d0d932748b66cc02cb51973
Judge Mongcal <134949328+judgemongcal@users.noreply.github.com>
2023-09-18 15:15:56 +0800

    Type Narrowing with Union types

--- 7-union-types/union.ts
+++ 7-union-types/union.ts
@@ -7,3 +7,32 @@ console.log(age('20'));
 
 let age2 : number | string = 23;
 age2 = '23';
+
+type Point = {
+    x: number,
+    y: number
+};
+
+type Loc = {
+    lat: number,
+    long: number
+};
+
+let coordinates : Point | Loc = {x: 23, y: 42};
+coordinates = {lat: 243, long: 421};
+
+// Type Narrowing with Union Types
+
+function calcTax(price: number | string, tax: number) : number {
+    // return price * tax;
+    // price.replace("$","")
+    if(typeof price === 'string') {
+        // price.replace('$','');
+        price = parseFloat(price.replace('$',''));
+    } 
+        return price * tax;
+    
+}
+    
+
+   
