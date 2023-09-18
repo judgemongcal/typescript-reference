8a331709dc0d502054d2b57f1db1dd8812114644
Judge Mongcal <134949328+judgemongcal@users.noreply.github.com>
2023-09-18 15:48:29 +0800

    Formatted exercise and edited loop in greet function

--- 7-union-types/unionExercise.ts
+++ 7-union-types/unionExercise.ts
@@ -1,45 +1,49 @@
 // Part 1
-let highScore : number | boolean;
+let highScore: number | boolean;
 
 // Part 2
-const stuffs : number[] | string[] = [];
+const stuffs: number[] | string[] = [];
 
 // Part 3
-type skillLevel = 'Beginner' | 'Intermediate' | 'Advnaced' | 'Expert'
+type skillLevel = 'Beginner' | 'Intermediate' | 'Advnaced' | 'Expert';
 
 // Part 4
 
 type SkiSchoolStudent = {
-    name:string,
-    age:number,
-    sport: 'ski' | 'snowboard',
-    level: skillLevel
+    name: string;
+    age: number;
+    sport: 'ski' | 'snowboard';
+    level: skillLevel;
 };
 
 // Part 5
 type RGB = {
-    r: number,
-    g: number,
-    b: number
-}
+    r: number;
+    g: number;
+    b: number;
+};
 
 type HSL = {
-    h: number,
-    s: number,
-    l: number
-}
+    h: number;
+    s: number;
+    l: number;
+};
 
-const colors : (RGB|HSL)[] = [];
+const colors: (RGB | HSL)[] = [];
 
 // Part 6
 const greet = (name: string | string[]) => {
-    if(typeof name === 'string') {
+    if (typeof name === 'string') {
         console.log(`Hello, ${name}`);
     } else {
-        name.forEach((n) => {
-            console.log(`Hello, ${name}.`);
-        })
+        // name.forEach((n) => {
+        //     console.log(`Hello, ${n}.`);
+        // });
+
+        for(let n of name) {
+            console.log(`Hello, ${n}`);
+        }
     }
-}
+};
 
 
