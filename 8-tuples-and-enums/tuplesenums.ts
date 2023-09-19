97f1b06757073ed195057e059f089c40449f8e68
Judge Mongcal <134949328+judgemongcal@users.noreply.github.com>
2023-09-19 17:17:46 +0800

    Enums

--- 8-tuples-and-enums/tuplesenums.ts
+++ 8-tuples-and-enums/tuplesenums.ts
@@ -1,17 +1,42 @@
-const stuff : string[] = ['assf','fsfs','dgdg']; // typical array
+// Tuples
 
+const stuff: string[] = ["assf", "fsfs", "dgdg"]; // typical array
 
 let myTuple: [number, string]; // Strict order of params
 
-myTuple = [23, 'fssf'];
+myTuple = [23, "fssf"];
 // myTuple = ['ssdsds', 23]; Won't work since it doesn't follow the data type of myTuple
 
-const color: [number, number, number] = [255,7,233];
+const color: [number, number, number] = [255, 7, 233];
 
 type HTTPResponse = [number, string];
 
-const goodRes : HTTPResponse = [200, 'Okay'];
-const badRes : HTTPResponse = [400, 'Not okay'];
-// goodRes[0] = '200'; 
+const goodRes: HTTPResponse = [200, "Okay"];
+const badRes: HTTPResponse = [400, "Not okay"];
+// goodRes[0] = '200';
 
-goodRes.push('242424'); // Works for some reason
+goodRes.push("242424"); // Works for some reason
+
+// Enums
+
+enum OrderStatus {
+    PENDING,
+    SHIPPED,
+    DELIVERED,
+    RETURNED,
+}
+
+const statusOne = OrderStatus.DELIVERED;
+
+function isDelivered(stat: OrderStatus) {
+    return stat === OrderStatus.DELIVERED;
+}
+
+isDelivered(OrderStatus.RETURNED);
+
+enum ArrowKeys {
+    UP = "up",
+    DOWN = "down",
+    LEFT = "left",
+    RIGHT = "right",
+}
