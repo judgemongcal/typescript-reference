c37161de6ee59590c4d5c200874c0953f4ec3ad3
Judge Mongcal <134949328+judgemongcal@users.noreply.github.com>
2023-09-19 16:53:22 +0800

    Tuples

---
+++ 8-tuples-and-enums/tuplesenums.ts
@@ -0,0 +1,9 @@
+const stuff : string[] = ['assf','fsfs','dgdg']; // typical array
+
+
+let myTuple: [number, string];
+
+myTuple = [23, 'fssf'];
+// myTuple = ['ssdsds', 23]; Won't work since it doesn't follow the data type of myTuple
+
+const color: [number, number, number] = [255,7,233];
