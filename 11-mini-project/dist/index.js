770a7212228765d33ad258a6da2b0ffff4972950
Judge Mongcal <134949328+judgemongcal@users.noreply.github.com>
2023-09-21 20:15:45 +0800

    Basic DOM functions

--- 11-mini-project/dist/index.js
+++ 11-mini-project/dist/index.js
@@ -1,13 +1,27 @@
 "use strict";
 const btn = document.getElementById("btn"); // ! - Non-Null Assertion
 const input = document.querySelector("#todo-input");
-btn.addEventListener("click", function () {
-    // alert("Clicked");
-    alert(input.value);
-    alert(input.value); // Another way for type assertion
-    input.value = "";
-});
+const form = document.querySelector("#todo-form");
+const list = document.querySelector("#todo-list");
+// btn.addEventListener("click", function () {
+// 	// alert("Clicked");
+// 	alert(input.value);
+// 	alert((<HTMLInputElement>input).value); // Another way for type assertion
+// 	input.value = "";
+// });
 // let mystery: unknown = "Hello World";
 // const numChars = (mystery as String).length; // Type Assertion just for this line
 // console.log(numChars);
 // Type Assertion in DOM
+const handleSubmit = (e) => {
+    e.preventDefault();
+    const newTodoText = input.value;
+    const newLi = document.createElement("li");
+    newLi.append(newTodoText);
+    list.appendChild(newLi);
+    const checkBox = document.createElement("input");
+    checkBox.type = "checkbox";
+    newLi.append(checkBox);
+    input.value = "";
+};
+form.addEventListener("submit", handleSubmit);
--- 11-mini-project/src/index.ts
+++ 11-mini-project/src/index.ts
@@ -1,11 +1,14 @@
 const btn = document.getElementById("btn")! as HTMLButtonElement; // ! - Non-Null Assertion
 const input = document.querySelector("#todo-input")! as HTMLInputElement;
-btn.addEventListener("click", function () {
-	// alert("Clicked");
-	alert(input.value);
-	alert((<HTMLInputElement>input).value); // Another way for type assertion
-	input.value = "";
-});
+const form = document.querySelector("#todo-form")! as HTMLFormElement;
+const list = document.querySelector("#todo-list")!;
+
+// btn.addEventListener("click", function () {
+// 	// alert("Clicked");
+// 	alert(input.value);
+// 	alert((<HTMLInputElement>input).value); // Another way for type assertion
+// 	input.value = "";
+// });
 
 // let mystery: unknown = "Hello World";
 
@@ -14,3 +17,17 @@ btn.addEventListener("click", function () {
 // console.log(numChars);
 
 // Type Assertion in DOM
+
+const handleSubmit = (e: SubmitEvent) => {
+	e.preventDefault();
+	const newTodoText = input.value;
+	const newLi = document.createElement("li");
+	newLi.append(newTodoText);
+	list.appendChild(newLi);
+	const checkBox = document.createElement("input");
+	checkBox.type = "checkbox";
+	newLi.append(checkBox);
+	input.value = "";
+};
+
+form.addEventListener("submit", handleSubmit);
