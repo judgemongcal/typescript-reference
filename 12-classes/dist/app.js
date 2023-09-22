25f7e38ea8283c65882d4a0910877366792b3031
Judge Mongcal <134949328+judgemongcal@users.noreply.github.com>
2023-09-22 17:52:05 +0800

    Class fields

--- 12-classes/dist/app.js
+++ 12-classes/dist/app.js
@@ -1,2 +1,23 @@
-"use strict";
-console.log("Hello World");
+class Player {
+	score = 0;
+	numLives = 10; // Class Fields
+	constructor(first, last) {
+		this.first = first;
+		this.last = last;
+	}
+	taunt() {
+		console.log("BOOYAH");
+	}
+
+	loseLife() {
+		this.numLives -= 1;
+	}
+}
+
+const player1 = new Player("John", "Doe");
+console.log(player1);
+const player2 = new Player("Roe", "Jogan");
+console.log(player2);
+console.log(player2.numLives);
+player2.numLives -= 2;
+console.log(player2.numLives);
