a77f957a469e55d7b653747f9f22b6d47929faae
Judge Mongcal <134949328+judgemongcal@users.noreply.github.com>
2023-09-23 15:35:55 +0800

    TS Generics

---
+++ 13-generics/dist/app.js
@@ -0,0 +1,6 @@
+"use strict";
+const nums = [];
+const num2 = [];
+const colors = [];
+const userName = document.querySelector("#username");
+console.log(userName);
---
+++ 13-generics/src/app.ts
@@ -0,0 +1,6 @@
+const nums: number[] = [];
+const num2: Array<number> = [];
+const colors: Array<string> = [];
+
+const userName = document.querySelector("#username");
+console.log(userName);
