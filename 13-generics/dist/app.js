b7dd7f4ad38a9640b1e828fe189ee252ca2016ab
Judge Mongcal <134949328+judgemongcal@users.noreply.github.com>
2023-09-23 16:01:53 +0800

    Generic Functions

--- 13-generics/dist/app.js
+++ 13-generics/dist/app.js
@@ -4,3 +4,28 @@ const num2 = [];
 const colors = [];
 const userName = document.querySelector("#username");
 console.log(userName);
+userName.value = "Hacked";
+const btn = document.querySelector(".btn");
+function numberIdentity(item) {
+    return item;
+}
+function stringIdentity(item) {
+    return item;
+}
+function booleanIdentity(item) {
+    return item;
+}
+// function identity(item: any): any{
+//     return item;
+// }
+function identity(item) {
+    return item;
+}
+identity("string");
+identity(34);
+function getRandomElement(list) {
+    const randIndex = Math.floor(Math.random() * list.length);
+    return list[randIndex];
+}
+console.log(getRandomElement([5, 6, 7, 8]));
+console.log(getRandomElement(["blue", "red", "orange", "green"]));
--- 13-generics/src/app.ts
+++ 13-generics/src/app.ts
@@ -2,5 +2,39 @@ const nums: number[] = [];
 const num2: Array<number> = [];
 const colors: Array<string> = [];
 
-const userName = document.querySelector("#username");
+const userName = document.querySelector<HTMLInputElement>("#username")!;
 console.log(userName);
+userName.value = "Hacked";
+
+const btn = document.querySelector<HTMLButtonElement>(".btn")!;
+
+function numberIdentity(item: number): number {
+	return item;
+}
+
+function stringIdentity(item: string): string {
+	return item;
+}
+
+function booleanIdentity(item: boolean): boolean {
+	return item;
+}
+
+// function identity(item: any): any{
+//     return item;
+// }
+
+function identity<Type>(item: Type): Type {
+	return item;
+}
+
+identity<string>("string");
+identity<number>(34);
+
+function getRandomElement<T>(list: T[]): T {
+	const randIndex = Math.floor(Math.random() * list.length);
+	return list[randIndex];
+}
+
+console.log(getRandomElement([5, 6, 7, 8]));
+console.log(getRandomElement(["blue", "red", "orange", "green"]));
