5cdbd03142fc6e85b84183d02cb162a34d0d8ba7
Judge Mongcal <134949328+judgemongcal@users.noreply.github.com>
2023-09-24 10:41:47 +0800

    Working with Axios Types

--- 14-type-declarations/dist/index.js
+++ 14-type-declarations/dist/index.js
@@ -1,1 +1,31 @@
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const axios_1 = __importDefault(require("axios"));
+// axios
+// 	.get<User>("https://jsonplaceholder.typicode.com/users/3")
+// 	.then((res) => {
+// 		console.log("Worked");
+// 		const { data } = res;
+// 		console.log(data);
+//         printUser(data);
+// 	})
+// 	.catch((e) => {
+// 		console.log("Error!", e);
+// 	});
+axios_1.default
+    .get("https://jsonplaceholder.typicode.com/users")
+    .then((res) => {
+    console.log("Worked");
+    res.data.forEach((element) => {
+        printUser(element);
+    });
+})
+    .catch((e) => {
+    console.log("Error!", e);
+});
+function printUser(user) {
+    console.log(`${user.name} - ${user.id} - ${user.phone} - ${user.company.name} `);
+}
---
+++ 14-type-declarations/src/index.ts
@@ -0,0 +1,52 @@
+import axios from "axios";
+
+interface User {
+	id: number;
+	name: string;
+	username: string;
+	email: string;
+	address: {
+		street: string;
+		suite: string;
+		city: string;
+		zipcode: string;
+		geo: { lat: string; lng: string };
+	};
+	phone: string;
+	website: string;
+	company: {
+		name: string;
+		catchPhrase: string;
+		bs: string;
+	};
+}
+
+// axios
+// 	.get<User>("https://jsonplaceholder.typicode.com/users/3")
+// 	.then((res) => {
+// 		console.log("Worked");
+// 		const { data } = res;
+// 		console.log(data);
+//         printUser(data);
+// 	})
+// 	.catch((e) => {
+// 		console.log("Error!", e);
+// 	});
+
+axios
+	.get<User[]>("https://jsonplaceholder.typicode.com/users")
+	.then((res) => {
+		console.log("Worked");
+		res.data.forEach((element) => {
+			printUser(element);
+		});
+	})
+	.catch((e) => {
+		console.log("Error!", e);
+	});
+
+function printUser(user: User) {
+	console.log(
+		`${user.name} - ${user.id} - ${user.phone} - ${user.company.name} `,
+	);
+}
